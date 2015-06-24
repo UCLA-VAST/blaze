132847a1588f1f4ca0fac82d2d8a39b9b00da719
Hao Yu (Cody) <comaniac0422@gmail.com>
2015-06-24 16:11:48 -0700

    RDD ACC initial commit

--- rddacc/app/test/run.sh
+++ rddacc/app/test/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export RDDACC_HOME=/curr/cody/Spark_FPGA/rddacc
+export ACC_HOME=/curr/cody/Spark_ACC/acc_runtime/rddacc
 
 spark-submit --class TestApp$1 \
-	--jars ${RDDACC_HOME}/target/RDD_ACC-1.0-SNAPSHOT.jar \
+	--jars ${ACC_HOME}/target/RDD_ACC-1.0-SNAPSHOT.jar \
 	--master local[*] \
 	target/testapp-0.0.0.jar
 
