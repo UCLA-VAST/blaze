239b1fdfbbcb32cb329720633c5c86d74013d4b0
Hao Yu (Cody) <comaniac0422@gmail.com>
2015-08-20 23:33:59 -0700

    Fix some path problems

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
-JAVA_HOME=
-BOOST_DIR=
-PROTOBUF_DIR=
-HADOOP_DIR=
+JAVA_HOME=$HOME/.local/jdk1.7.0_79/
+BOOST_DIR=/curr/diwu/tools/boost_1_55_0/install
+PROTOBUF_DIR=/curr/diwu/tools/protobuf-2.5.0/build/install
+HADOOP_DIR=/curr/diwu/tools/hadoop/hadoop-2.5.2
 
-export BLAZE_HOME=`pwd`/blaze
+export BLAZE_HOME=`pwd`/accrdd
 export LD_LIBRARY_PATH=$HADOOP_DIR/lib/native:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$JAVA_HOME/jre/lib/amd64/server:$LD_LIBRARY_PATH
 
