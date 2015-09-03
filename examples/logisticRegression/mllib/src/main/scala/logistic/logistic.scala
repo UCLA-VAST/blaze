10f605e0d34e7c889d8f0a3748a549f5f9b30ef6
Di Wu <allwu@cs.ucla.edu>
2015-09-03 16:07:09 -0700

    added mllib impl of logistic regression, and a corresponding acc_impl that calculate gradient and loss in the same way as mllib.optimization.Gradient

---
+++ examples/logisticRegression/mllib/src/main/scala/logistic/logistic.scala
@@ -0,0 +1,57 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+import org.apache.spark.mllib.classification.{LogisticRegressionWithLBFGS, LogisticRegressionModel}
+import org.apache.spark.mllib.evaluation.MulticlassMetrics
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.util.MLUtils
+import org.apache.spark.blaze._
+
+object LogisticRegression {
+
+  def main(args : Array[String]) {
+
+    var conf = new SparkConf().setAppName("LogisticRegression")
+    val sc = new SparkContext(conf)
+
+    if (args.length < 2) {
+      System.err.println("Usage: LogisticRegression <train_file> <test_file> <part_num>")
+      System.exit(1)
+    }
+
+    val reps = args(2).toInt
+
+    val train = sc.textFile(args(0)).map( line => {
+        var parts = line.split(',') 
+        LabeledPoint(parts(0).toDouble, 
+            Vectors.dense(parts(1).split(' ').map(_.toDouble)))
+        }).repartition(reps).cache()
+
+    val test = sc.textFile(args(1)).map( line => {
+        var parts = line.split(',') 
+        LabeledPoint(parts(0).toDouble, 
+            Vectors.dense(parts(1).split(' ').map(_.toDouble)))
+        });
+
+    val model = new LogisticRegressionWithLBFGS()
+      .setNumClasses(10)
+      .run(train)
+
+    println("train finished.")
+
+    // Compute raw scores on the test set.
+    val predictionAndLabels = test.map { case LabeledPoint(label, features) =>
+      val prediction = model.predict(features)
+        (prediction, label)
+      }
+    
+    // Get evaluation metrics.
+    val metrics = new MulticlassMetrics(predictionAndLabels)
+    val precision = metrics.precision
+    println("Precision = " + precision)
+
+  }
+}
+
+
