5b12900ff047a69a65a3ed4ae897c3a39ff89c1a
Di Wu <allwu@falcon-computing.com>
2015-10-28 15:53:33 -0700

    add easy config script to deploy example accelerator with application

--- package/examples/logisticRegression/app/run.sh
+++ package/examples/logisticRegression/app/run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-SPARK_HOME=../../../spark-1.4.0-falcon
+if [ -z $FCS_RT_ROOT ]; then
+  FCS_RT_ROOT=../../..
+fi
+if [ ! -f "$FCS_RT_ROOT/nam/bin/nam_daemon" ]; then
+  echo "FCS_RT_ROOT seems incorrect, please set it to the root directory of FCS_RUNTIME"
+  exit -1
+fi
+SPARK_HOME=$FCS_RT_ROOT/spark-1.4.0-falcon
 $SPARK_HOME/bin/spark-submit --class LogisticRegression \
 	--driver-memory 8G \
 	--executor-memory 4G \
