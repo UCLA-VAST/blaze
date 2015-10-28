0f239cb602c2dcdc8bc895b57f84472fdea4a5b4
Muhuan Huang <mhhuang@cs.ucla.edu>
2015-10-28 16:16:54 -0700

    fix SPARK_HOME

--- package/examples/logisticRegression/app/run.sh
+++ package/examples/logisticRegression/app/run.sh
@@ -7,7 +7,7 @@ if [ ! -f "$FCS_RT_ROOT/nam/bin/nam_daemon" ]; then
   echo "FCS_RT_ROOT seems incorrect, please set it to the root directory of FCS_RUNTIME"
   exit -1
 fi
-SPARK_HOME=$FCS_RT_ROOT/spark-1.4.0-falcon
+SPARK_HOME=$FCS_RT_ROOT/spark-1.4.0-bin-fcs
 $SPARK_HOME/bin/spark-submit --class LogisticRegression \
 	--driver-memory 8G \
 	--executor-memory 4G \
