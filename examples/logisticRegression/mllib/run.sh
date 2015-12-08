c19c77a2f907ad330603b0a693cc7c5377490ceb
Di Wu <allwu@falcon-computing.com>
2015-12-07 18:20:21 -0800

    minor changes in spark run script

--- examples/logisticRegression/mllib/run.sh
+++ examples/logisticRegression/mllib/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/curr/diwu/prog/spark-1.4.1/bin/spark-submit --class LogisticRegression \
+/curr/diwu/prog/blaze/spark-1.4.0/bin/spark-submit --class LogisticRegression \
 	--driver-memory 16G \
 	--executor-memory 16G \
 	--master local[*] \
