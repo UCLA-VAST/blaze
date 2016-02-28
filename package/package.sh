5b590f9c19e6d625776d5154aec554548c9bd67c
Di Wu <allwu@falcon-computing.com>
2016-02-28 11:02:17 -0800

    rename setup environment script in package

--- package/package.sh
+++ package/package.sh
@@ -8,12 +8,12 @@ rsync -aLv hadoop-2.6.0-bin-fcs.tar.gz fcs_runtime
 rsync -aLv fcslm-lin64.tar.gz fcs_runtime
 rsync -aLrv --exclude=".*" bin fcs_runtime
 rsync -aLrv --exclude=".*" nam fcs_runtime
-rsync -aLrv --exclude=".*" lib fcs_runtime
-rsync -aLrv --exclude=".*" docs fcs_runtime
+#rsync -aLrv --exclude=".*" lib fcs_runtime
+#rsync -aLrv --exclude=".*" docs fcs_runtime
 rsync -aLrv --exclude=".*" examples fcs_runtime
 rsync -aLrv --exclude=".*" include fcs_runtime
 rsync -aLrv --exclude=".*" install.sh fcs_runtime
-rsync -aLrv --exclude=".*" setup.sh fcs_runtime
+rsync -aLrv --exclude=".*" setup-env.sh fcs_runtime
 rsync -aLrv --exclude=".*" README.md fcs_runtime
 
 tar zcf fcs_runtime.tar.gz fcs_runtime
