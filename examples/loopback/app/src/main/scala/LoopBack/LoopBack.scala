5a3aa05865a30161e096bc41c9b2c8d2dbea2358
Di Wu <allwu@falcon-computing.com>
2016-07-18 16:31:38 -0700

    Added Altera OpenCL support in platforms, created working example flow using loopback

--- examples/loopback/app/src/main/scala/LoopBack/LoopBack.scala
+++ examples/loopback/app/src/main/scala/LoopBack/LoopBack.scala
@@ -1,20 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
@@ -29,7 +12,7 @@ import org.apache.spark.blaze._
 class LoopBack() 
   extends Accelerator[Array[Double], Array[Double]] {
 
-  val id: String = "NLB"
+  val id: String = "LoopBack"
   def getArg(idx: Int): Option[BlazeBroadcast[_]] = None
   def getArgNum(): Int = 0
 
@@ -75,13 +58,15 @@ object LoopBack {
 
     if (src.deep == dst.deep) {
       println("results correct") 
+      acc.stop()
     }
     else {
       println("results incorrect") 
       println(src.deep.mkString("\n"))
       println(dst.deep.mkString("\n"))
+      acc.stop()
+      System.exit(1)
     }
-    acc.stop()
   }
 
   def get_spark_context(appName : String) : SparkContext = {
---
+++ platforms/altr_opencl/OpenCLPlatform.h
@@ -0,0 +1,68 @@
+#ifndef OPENCL_PLATFORM_H
+#define OPENCL_PLATFORM_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <stdexcept>
+
+#include <CL/opencl.h>
+
+#include "OpenCLCommon.h"
+#include "OpenCLBlock.h"
+#include "OpenCLEnv.h"
+#include "blaze/Platform.h"
+
+namespace blaze {
+
+class OpenCLPlatform : public Platform {
+
+public:
+
+  OpenCLPlatform(std::map<std::string, std::string> &conf_table);
+
+  ~OpenCLPlatform();
+
+  virtual TaskEnv_ptr getEnv(std::string id);
+
+  virtual DataBlock_ptr createBlock(
+      int num_items, 
+      int item_length,
+      int item_size, 
+      int align_width = 0,
+      int flag = BLAZE_INPUT_BLOCK);
+
+  virtual void createBlockManager(size_t cache_limit, size_t scratch_limit);
+  virtual BlockManager* getBlockManager();
+
+  void addQueue(AccWorker &conf);
+  void removeQueue(std::string id);
+
+  void changeProgram(std::string acc_id);
+
+  cl_kernel& getKernel();
+
+private:
+
+  int load_file(const char* filename, char** result);
+  
+  OpenCLEnv*  env;
+  TaskEnv_ptr env_ptr;
+
+  std::string curr_acc_id;
+  cl_program  curr_program;
+  cl_kernel   curr_kernel;
+
+  std::map<std::string, std::pair<int, unsigned char*> > bitstreams;
+  std::map<std::string, std::string> kernel_list;
+  //std::map<std::string, cl_kernel>  kernels;
+};
+
+extern "C" Platform* create(
+    std::map<std::string, std::string> &config_table);
+
+extern "C" void destroy(Platform* p);
+
+} // namespace blaze
+
+#endif
